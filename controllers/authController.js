ecedf616ff5f3c10b87acffaee38cd1e85f88ad6
kalpshah01 <kalpshah962005@gmail.com>
2026-06-25 17:12:51 +0530

    encrypted passowrd

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,5 @@
 const User = require("../model/userModel");
-
+const bcrypt=require("bcrypt");
 const testController = (req, res) => {
   res.redirect("/auth/signup");
 };
@@ -26,44 +26,53 @@ const signinController = (req, res) => {
 };
 
 
-const registerController = async (req, res) => {
+  const registerController = async (req, res) => {
   try {
-    console.log("req body is", req.body);
-    let { name, email, password } = req.body;
-    let newUser = User({
+    const { name, email, password } = req.body;
+
+    const hashpass = await bcrypt.hash(password, 12);
+// console.log(hashpass);
+    const newUser = new User({
       name,
       email,
-      password,
+      password: hashpass,
     });
 
     await newUser.save();
-    
+
     res.redirect("/auth/signin");
   } catch (err) {
-    console.log("Error in Registeration", err);
-    res.render("");
+    console.log(err);
+    res.redirect("/auth/signup");
   }
 };
 
-const loginController=async(req,res)=>{
-console.log("login data is",req.body);
-let{email,password}=req.body;
-let user=await User.findOne({email});
-console.log(user);
-if(user){
-    if(user.password === password){
-        res.cookie("user",user._id);
-        res.redirect("/dashboard");
+
+  const loginController=async(req,res)=>{
+  console.log("login data is",req.body);
+  let{email,password}=req.body;
+  let user=await User.findOne({email});
+  console.log(user);
+  if(user){
+      bcrypt.compare(password, user.password, (err, result) => {
+    if (err) {
+        return res.redirect("/auth/signin");
     }
-    else{
+
+    if (result) {
+        res.cookie("user", user._id);
+        res.redirect("/dashboard");
+    } else {
         res.redirect("/auth/signin");
     }
-}
-else{
-    res.redirect("/auth/signup");
-}
-//res.render('');
-}
+   });
+      
+  }
+  else{
+      res.redirect("/auth/signup");
+  }
+  //res.render('');
+  }
 const logoutController = (req,res)=>{
     
     res.clearCookie("user");
